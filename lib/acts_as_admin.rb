6b0bdbde5481ee8d2f3e5a495aa17b7435633815
jeet <jeet.ajay@gmail.com>
2010-06-01 16:18:38 +0530

    misc changes

--- lib/acts_as_admin.rb
+++ lib/acts_as_admin.rb
@@ -1,1 +1,52 @@
 # ActsAsAdmin
+module  Acts
+  module Admin
+    CALLBACKS = []
+
+    def self.included(base) # :nodoc:
+      base.extend ClassMethods
+    end
+
+    module  ClassMethods
+      def acts_as_admin(options = {})
+        return if self.included_modules.include?(Acts::Admin::InstanceMethods)
+
+        class_inheritable_reader :non_admin_columns
+        class_inheritable_reader :admin_enabled
+        if options[:only]
+          except = self.column_names - options[:only].flatten.map(&:to_s)
+        else
+          except = [self.primary_key, inheritance_column, 'lock_version',
+                    'created_at', 'updated_at', 'created_on', 'updated_on']
+          except |= Array(options[:except]).collect(&:to_s) if options[:except]
+        end
+        write_inheritable_attribute :non_admin_columns, except
+        Administrator.admin_class_names << self.to_s
+        extend Acts::Admin::SingletonMethods
+        include Acts::Admin::InstanceMethods
+
+        write_inheritable_attribute :admin_enabled, true
+      end
+    end
+
+
+    module InstanceMethods
+      def admin_attributes
+        attributes.except(*non_admin_columns)
+      end
+
+
+    end
+
+    private
+
+    module SingletonMethods
+      def admin_columns
+        self.columns.select { |c| !non_admin_columns.include?(c.name) }
+      end
+    end
+
+
+
+  end
+end
